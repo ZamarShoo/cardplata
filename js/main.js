6429d0ed170340fbfc0855881b60415d52ad44a1
Ivan Shulga <shg.ivan97@gmail.com>
2020-08-20 22:00:22 +0300

    mobile

---
+++ js/main.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+    if ( $(window).width() < 1100 ) {
+        $(".advantages--first-slide").owlCarousel({
+            nav: false,
+            dots: false,
+            items: 4,
+            center: true,
+            margin:30,
+            responsive: {
+                0: {
+                    center: true,
+                    items: 1,
+                    stagePadding: 60,
+                },
+                500: {
+                    items: 2,
+                    stagePadding: 20,
+                },
+                780: {
+                    items: 3,
+                    stagePadding: 20,
+                }
+            }
+        });
+
+        $(".activation--dawn").owlCarousel({
+            nav: false,
+            dots: false,
+            margin:40,
+            center: true,
+            responsive: {
+                0: {
+                    center: true,
+                    items: 1,
+                    stagePadding: 90,
+                },
+                780: {
+                    items: 2,
+                    stagePadding: 90,
+                }
+            }
+        });
+
+        $(".all-section").owlCarousel({
+            nav: false,
+            dots: false,
+            margin:30,
+            center: true,
+            responsive: {
+                0: {
+                    center: true,
+                    items: 1,
+                    stagePadding: 60,
+                },
+                500: {
+                    items: 2,
+                    stagePadding: 20,
+                },
+                780: {
+                    items: 3,
+                    stagePadding: 20,
+                }
+            }
+        });
+    
+      } else {
+        $('.advantages--first-slide').addClass('off');
+        $('.activation--dawn').addClass('off');
+        $('.all-section').addClass('off');
+      };
+
+      
+  });
+
+
+
+$(".review-bottom").owlCarousel({
+    nav: true,
+    dots: false,
+    navContainer: '#reviewNavContainer',
+    stagePadding: 0,
+    responsive: {
+        0: {
+            center: true,
+            items: 1
+        },
+        740: {
+            items: 2
+        },
+        1100: {
+            items: 3
+        }
+    }
+});
+
+$(".news").owlCarousel({
+    nav: true,
+    dots: false,
+    navContainer: '#newsNavContainer',
+    stagePadding: 0,
+    responsive: {
+        0: {
+            center: true,
+            items: 1
+        },
+        740: {
+            items: 2
+        },
+        1100: {
+            items: 3
+        }
+    }
+});
